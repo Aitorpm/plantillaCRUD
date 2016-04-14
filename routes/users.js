a5cf26e76828629cd37e2fdd4a6fce301921c08b
Aitor Piñeyro <itor95@gmail.com>
2016-04-14 11:43:02 +0200

    COn el CRUD hecho en mongoose

--- routes/users.js
+++ routes/users.js
@@ -6,21 +6,30 @@ module.exports = function (app) {
     AllUsers = function (req, res) {
         User.find(function (err, users) {
             if (err) res.send(500, err.message);
-
-            console.log('GET /contactlist')
+            console.log('GET /contactlist');
             res.status(200).json(users);
         });
     };
 
+    //GET - Get only a user
+    getUser = function(req, res){
+        User.findById(req.params.id, function (err, user) {
+            console.log('DELETE usuario');
+            if (err) res.send(500, err.message);
+            console.log('GET /contactlist/:id');
+            res.status(200).json(user);
+
+        });
+    }
+
     //POST - Add User in DB
     addUser = function (req, res) {
         console.log('POST');
-        console.log(req.body);
 
         var users = new User({
-            name: {type : String},
-            email:   { type: String},
-            number: {type: String},
+            name: req.body.name,
+            email:   req.body.email,
+            number: req.body.number,
         })
 
         users.save(function (err, users) {
@@ -30,7 +39,42 @@ module.exports = function (app) {
         });
     };
 
+    //DELETE - Delete a User with specified Name
+    deleteUser = function (req, res) {
+        User.findById(req.params.id, function (err, user) {
+            console.log('DELETE usuario');
+            return user.remove(function (err) {
+                if (!err) {
+                    console.log("usuario eliminado");
+                    return res.send('');
+                } else {
+                    console.log(err);
+                }
+            });
+        });
+    }
+
+    //UPDATE - Update a User
+    updateUser = function (req,res) {
+        User.findById(req.params.id, function (err, user) {
+            console.log('PUT');
+            console.log(req.params.id);
+
+            user.name = req.body.name;
+            user.email = req.body.email;
+            user.number = req.body.number;
+
+            user.save(function (err) {
+                if (err) return res.send(500, err.message);
+                res.status(200).json(user);
+            });
+        });
+    }
+
     //endpoints
     app.get('/contactlist/',AllUsers);
+    app.get('/contactlist/:id',getUser);
     app.post('/contactlist/', addUser);
+    app.delete('/contactlist/:id', deleteUser);
+    app.put('/contactlist/:id', updateUser);
 }
--- server.js
+++ server.js
@@ -2,8 +2,6 @@
 
 var express = require('express');
 var app = express();
-var mongojs = require('mongojs');
-//var db = mongojs('contactlist', ['contactlist']);
 var methodOverride  = require("method-override");
 var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
@@ -25,50 +23,6 @@ app.use(express.static(__dirname + "/public")); // FRONT-END---> aqui es donde t
 // API Rutas
 routes = require('./routes/users')(app);
 
-/*
-app.get('/contactlist', function (req, res) {
-  console.log('I received a GET request');
 
-  db.contactlist.find(function (err, docs) {
-    console.log(docs);
-    res.json(docs);
-  });
-});
-
-app.post('/contactlist', function (req, res) {
-  console.log(req.body);
-  db.contactlist.insert(req.body, function(err, doc) {
-    res.json(doc);
-  });
-});
-
-app.delete('/contactlist/:id', function (req, res) {
-  var id = req.params.id;
-  console.log(id);
-  db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
-    res.json(doc);
-  });
-});
-
-app.get('/contactlist/:id', function (req, res) {
-  var id = req.params.id;
-  console.log(id);
-  db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
-    res.json(doc);
-  });
-});
-
-app.put('/contactlist/:id', function (req, res) {
-  var id = req.params.id;
-  console.log(req.body.name);
-  db.contactlist.findAndModify({
-    query: {_id: mongojs.ObjectId(id)},
-    update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
-    new: true}, function (err, doc) {
-      res.json(doc);
-    }
-  );
-});
-*/
 app.listen(3000);
 console.log("Server running on port 3000");
